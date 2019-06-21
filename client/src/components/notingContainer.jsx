494c7a0a9ec55ef7136fe449b4c58c20e705e87f
Valentin Misiaszek <mowerv6@gmail.com>
2019-06-21 16:19:29 +0200

    Rework noting state && install moment.js in client

--- client/src/components/notingContainer.jsx
+++ client/src/components/notingContainer.jsx
@@ -5,31 +5,46 @@ import "../styles/notingContainer.css";
 
 class NotingContainer extends Component {
   state = {
-    note: "",
-    color: "",
+    note: {
+      value: "",
+      color: ""
+    },
     hoursLeft: 5
   };
 
+  handleNoteSelect = (value, color) => {
+    const note = { ...this.state.note };
+    note.value = value;
+    note.color = color;
+    this.setState({ note: note });
+  };
+
   render() {
-    const colorList = ["vbad-day", "bad-day", "med-day", "gd-day", "vgd-day"];
+    const noteList = [
+      { value: 1, color: "vbad-day" },
+      { value: 2, color: "bad-day" },
+      { value: 3, color: "med-day" },
+      { value: 4, color: "gd-day" },
+      { value: 5, color: "vgd-day" }
+    ];
     return (
       <div className="row ">
         <div className="col-md-12">
           <div className="container noting-container">
             <span className="note-ur-day">Note your day</span>
             <div className="container circle-container">
-              {colorList.map(color => (
+              {noteList.map(note => (
                 <div
-                  key={color}
-                  onClick={() => this.setState({ color: color })}
+                  key={note.value}
+                  onClick={() => this.handleNoteSelect(note.value, note.color)}
                 >
-                  <NoteCell color={color} />
+                  <NoteCell color={note.color} />
                 </div>
               ))}
             </div>
-            {this.state.note && (
+            {this.state.note.value && (
               <Button variant="primary" className="validate-btn">
-                Validate <NoteCell color={this.state.note} />
+                Validate <NoteCell color={this.state.note.color} />
               </Button>
             )}
             <span className="hours-left">
