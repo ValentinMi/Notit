92474e51962eb09f961c4cd1468074fc11cc2de7
Valentin Misiaszek <mowerv6@gmail.com>
2019-06-29 22:09:34 +0200

    add optiosn to start from 0

--- client/src/components/commons/graph/barGraph.jsx
+++ client/src/components/commons/graph/barGraph.jsx
@@ -2,12 +2,20 @@ import React from "react";
 import { Bar } from "react-chartjs-2";
 
 const BarGraph = ({ graphData }) => {
-  console.log(graphData);
   return (
     <Bar
       data={graphData}
       options={{
-        maintainAspectRatio: false
+        maintainAspectRatio: false,
+        scales: {
+          yAxes: [
+            {
+              ticks: {
+                beginAtZero: true
+              }
+            }
+          ]
+        }
       }}
     />
   );
