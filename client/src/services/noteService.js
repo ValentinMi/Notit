1c43ff63f714756031392ab261b9ed1c32332266
Valentin Misiaszek <mowerv6@gmail.com>
2019-07-06 19:50:03 +0200

    Add specific notes services

--- client/src/services/noteService.js
+++ client/src/services/noteService.js
@@ -22,6 +22,10 @@ function getUserNotes() {
   return http.get(apiEndPoint + "/mine");
 }
 
+//////////////////
+// CURRENT DATA //
+//////////////////
+
 // GET CURRENT WEEK NOTES
 function getCurrentWeekNotes() {
   return http.get(apiEndPoint + "/mine/week");
@@ -37,11 +41,33 @@ function getCurrentYearNotes() {
   return http.get(apiEndPoint + "/mine/year");
 }
 
+///////////////////
+// SPECIFIC DATA //
+///////////////////
+
+// GET SPECIFIC WEEK NOTES
+function getSpecificWeekNotes(week, year) {
+  return http.get(`${apiEndPoint}/mine/${year}/${week}`);
+}
+
+// GET SPECIFIC MONTH NOTES
+function getSpecificMonthNotes(month, year) {
+  return http.get(`${apiEndPoint}/mine/${year}/${month}`);
+}
+
+// GET SPECIFIC YEAR NOTES
+function getSpecificYearNotes(year) {
+  return http.get(`${apiEndPoint}/mine/${year}`);
+}
+
 export default {
   getAllNotes,
   getUserNotes,
   getCurrentWeekNotes,
   getCurrentMonthNotes,
   getCurrentYearNotes,
+  getSpecificWeekNotes,
+  getSpecificMonthNotes,
+  getSpecificYearNotes,
   saveNote
 };
